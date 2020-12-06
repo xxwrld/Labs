6d3cacfcf7ae650475b25204a83d87e7c4ca5d89
xxwrld <xxwrld@users.noreply.github.com>
2020-12-06 18:01:37 +0200

    Update

--- Python files/LAB7/LAB7 TASK5.py
+++ Python files/LAB7/LAB7 TASK5.py
@@ -3,8 +3,10 @@ a = [[float(input('a[{0}][{1}]='.format(i, j))) for j in range(n)] for i in rang
 b = []
 for i in range(n):
     if len([el for el in a[i] if el >= 0]) == n:
-        b.extend(a[i])
-d = 1
-for i in range(len(b)):
-    d *= b[i]
-print(d)
+        b.append(a[i])
+for j in range(len(b)):
+    d = 1
+    for i in range(len(b[j])):
+        d *= b[j][i]
+    b[j]=d
+print(b)
