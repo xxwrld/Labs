4b76430762fc147816b3671182d332dab52295f7
xxwrld <xxwrld@users.noreply.github.com>
2020-12-20 23:51:22 +0200

    Update

--- Python files/LAB10/LAB10 TASK2.py
+++ Python files/LAB10/LAB10 TASK2.py
@@ -1,29 +1,43 @@
 import datetime
 
 
-class Car:
-    def __init__(self, _a, _b, _c, _f, _k, _n, _y):
-        self.a = _a  # manufacturer(Ford, Dodge...)
-        self.b = _b  # about_manufacturer
-        self.c = _c  # about_seller
+class manufacturer:
+    def __init__(self, _a, _b, _c, _d):
+        self.a = _a  # name_manufacturer
+        self.b = _b  # foundation_year
+        self.c = _c  # phone_number
+        self.d = _d  # production_volumes
+class seller:
+    def __init__(self, _g, _j, _l, _q):
+        self.g = _g  # name_manufacturer
+        self.j = _j  # foundation_year
+        self.l = _l  # phone_number
+        self.q = _q  # production_volumes
+class owner:
+    def __init__(self, _f, _p):
         self.f = _f  # about_owner
+        self.p = _p  # owner_id
+    def change(self,x,y):
+        self.f = x
+        self.p = y
+
+class Car:
+    def __init__(self, _k, _n, _t, _a, _b, _c, _d, _g, _j, _l, _q, _f, _p, _y):
         self.k = _k  # color
         self.n = _n  # number
-        self.y = _y  # year
+        self.t = _t  # name
+        self.manufacturer = manufacturer(_a, _b, _c, _d)
+        self.seller = seller(_g, _j, _l, _q)
+        self.owner = owner( _f, _p)
+        self.age = _y
+
+    def changes_owner(self, x, y):
+        self.owner.change(x,y)
+
 
     def car_age(self):
         now = datetime.date.today()
-        delta = now - self.y
+        delta = now - self.age
         return delta
 
-    def changes_owner(self, x, y):
-        self.f = [x, y]
 
-###tests###
-##Owner
-# f=Car(0,0,0,0,0,0,0)
-# f.changes_owner(5,6)
-# print(f.f)
-##Car_Age
-# f = Car( datetime.date(1997, 6, 23))
-# print(f.car_age())
